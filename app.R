090b443eef7a7fb97f386204e128825f25c4d589
Glen Myrland <glenmyrland@gmail.com>
2017-02-13 00:05:08 -0500

    Add template files and build generate_spreadsheet function

--- app.R
+++ app.R
@@ -1,21 +1,71 @@
 library(shiny)
 library(dplyr)
+library(XLConnect)
 
 ## Defaults
 group_choices <- c("Pizza", "Pop", "Chips")
 default_custom_text <- "default"
 
 ## Template Information
+data_path <- "www/"
 templates <- data_frame(
     name = c("Type A", "Type B", "Type C"),
-    path = c("TemplateA.xlsx", "TemplateB.xlsx", "TemplateC.xlsx"),
-    img  = c("a.png", "b.png", "c.png")
+    path = paste0(data_path, c("TemplateA.xlsx", "TemplateB.xlsx", "TemplateC.xlsx")),
+    img  = paste0(data_path, c("a.png", "b.png", "c.png"))
 )
 
 ## Generate spreadsheet based on form inputs
 generate_spreadsheet <- function(options, ...) {
+    # User choices...
+    template_path <- options$template_path
+    image_path <- options$image_path
+    custom_text <- options$custom_text
+    choices <- options$choices
     
-    return(list(status=0, message="Success"))
+    # Sanity checks
+    if (!file.exists(template_path)) {
+        return(list(status=1, message=paste("Couldn't find the template:", template_path)))
+    }
+    if (!file.exists(image_path)) {
+        return(list(status=1, message=paste("Couldn't find the image:", image_path)))
+    }
+    
+    # Path to write
+    path <- tempfile(pattern = "file", tmpdir = tempdir(), fileext = ".xlsx")
+    
+    # Copy chosen template
+    file.copy(template_path, path)
+    
+    # Edit workbook based on selected options
+    wb <- loadWorkbook(path)
+    ws <- "Sheet1" # Edit as req'd
+    
+    # Formatting
+    csHlight = createCellStyle(wb, name = "highlight")
+    setFillPattern(csHlight, fill = XLC$FILL.SOLID_FOREGROUND)
+    setFillForegroundColor(csHlight, color = XLC$COLOR.CORNFLOWER_BLUE)
+    setCellStyle(wb, sheet=ws, row=10, col=seq(8,11), cellstyle = csHlight)
+    csBorder = createCellStyle(wb, name = "border")
+    setBorder(csBorder, side=c("top", "bottom", "left", "right"), type=XLC$BORDER.THIN, color = XLC$COLOR.BLACK)
+    #setCellStyle(wb, sheet=ws, formula="Sheet1!$H$10", cellstyle = csBorder)
+    writeWorksheet(wb, "Here's some more formatting created programmatically", sheet=ws, startRow=9, startCol=8, header=FALSE)
+    setCellStyle(wb, sheet=ws, row=11, col=seq(8,11), cellstyle = csBorder)
+
+    # Image
+    createName(wb, name="img", formula="Sheet1!$A$4:$B$8")
+    addImage(wb, filename = image_path, name="img", originalSize = FALSE)
+    writeWorksheet(wb, "Custom image based on selected template", sheet=ws, startRow=9, startCol=1, header=FALSE)
+    
+    # Custom text
+    writeWorksheet(wb, c("This was your custom text: ", custom_text), sheet=ws, startRow = 13, startCol = 1, header=FALSE)
+    
+    # Multiple value selection
+    writeWorksheet(wb, data.frame(Choices=choices), sheet=ws, startRow = 16, startCol = 1, header=TRUE, rownames = TRUE)
+    
+    # Save result
+    saveWorkbook(wb)
+    
+    return(list(status=0, message="Success", path=path))
 }
 
 ## Shiny UI
@@ -50,9 +100,17 @@ server <- function(input, output, session) {
     output$results <- renderText("Status: Nothing to report")
     observeEvent(input$generate, {
         res <- generate_spreadsheet(
-            options = list()
+            options = list(
+                template_path = templates$path[templates$name == input$template],
+                image_path = templates$img[templates$name == input$template],
+                custom_text = input$custom,
+                choices = input$choices
+            )
         )
         output$results <- renderText(paste("Status:", res$message))
+        if (res$status == 0) {
+            system2("open", paste0('"', res$path, '"'))
+        }
     })
     session$onSessionEnded(function() stopApp(returnValue=NULL))
 }
